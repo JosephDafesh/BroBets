816909cd32c1701bb9656d1f11ae3a91b6e40f68
Joseph Dafesh <jodafesh@gmail.com>
2024-02-12 12:18:35 -0800

    added injdex and app

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+function App(){
+    return (
+        <div>hi</div>
+    )
+}
+
+export default App
