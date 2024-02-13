b4f9f67fa72b178a5301072199dd17e35a4bfcc9
Jarod Crawford <jarodcrawford@Jarods-Air.attlocal.net>
2024-02-13 12:13:27 -0600

    fixes package dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-<<<<<<< HEAD
   // mode: "production",
-=======
-  mode: 'production',
->>>>>>> dev
   entry: './client/index.js',
   output: {
     filename: 'bundle.js',
