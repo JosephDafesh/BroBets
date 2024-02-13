3c3f29a602a6a107d3be4a8a8df15eeee2a47c68
Ariel Maor <arielmaor@Ariels-MacBook-Pro.local>
2024-02-12 17:55:03 -0800

    Add basic user controllers and api endpoints

---
+++ server/controllers/userController.js
@@ -0,0 +1,41 @@
+const db = require('../models/db');
+const bcrypt = require('bcrypt');
+
+const userController = {};
+
+userController.createUser = async (req, res, next) => {
+    const {} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    db.query()
+    .then(result => {
+        console.log('result:', result)
+        res.locals.userCreated = 'User created successfully';
+        return next();
+    })
+    .catch(err => next({
+        log: 'Express error handler caught unknown middleware error',
+        message: { err: 'An error occurred' },
+        err
+    }))
+};
+
+userController.verifyUser = async (req, res, next) => {
+    const { username, password } = req.body;
+    const inputPassword = password;
+
+    db.query()
+    .then(async (hashedPassword) => {
+        res.locals.passwordMatches = await bcrypt.compare(inputPassword, hashedPassword.rows[0].password);
+        if (res.locals.passwordMatches) {
+            res.cookie('token', 'user');
+            return next();
+        }
+    })
+    .catch (err => next({
+        log: 'Express error handler caught in usercontroller.verifyUser middleware',
+        message: { err: 'An error occurred during login' },
+        err
+    }))
+};
---
+++ server/routes/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+
+router.post('signin', (req, res) => {
+    return res.status(200).json();
+})
+
+router.post('signup', (req,res) => {
+    return res.status(200).json();
+})
